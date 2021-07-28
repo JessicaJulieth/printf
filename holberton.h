884436619e2acf0ff32cf0e7daa3901eeb0f9816
JessicaJulieth <3497@holbertonschool.com>
2021-07-27 23:06:53 -0500

    Organizing the prototypes

--- holberton.h
+++ holberton.h
@@ -1,36 +1,29 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
-
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdarg.h>
 
 /**
- *struct op - Struct op
- * @op:opator
- * @function: It is a fuction to process
+ * struct op - This is the structure for the print
+ * @op: Operator that print the characters
+ * @function:Get the functions pointer to process
  */
 
 typedef struct op
 {
 	char *op;
-	int (*function)(int a, int b);
-}
-op_t;
+	int (*function)(va_list);
+} op_t;
 
-/** prototype main*/
-int _printf(const char *format, ...);
-
-
-/**
- * op_character - character
- * @list: List
- * Return: (0)
- */
+/* En este bloque tendremos los protptipos de las funciones de casso */
+/* C S D I % */
 int op_character(va_list list);
-int (*get_op_func(char *Character))(int Number1, int Number2);
-int _printf(const char *format, ...);
+int op_string(va_list list);
+int op_integer(va_list list);
 
-int _strlen(char *s);
+/* They are the fuctions principal*/
+int (*get_op_func(const char *Character))(va_list);
+int _printf(const char *format, ...);
 
 #endif
