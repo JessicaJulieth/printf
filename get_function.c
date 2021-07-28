e9fc566d935b292b9a2ec6f55bc986c5ec4d88f0
JessicaJulieth <3497@holbertonschool.com>
2021-07-28 18:49:18 -0500

    Changes whit betty V 3.3

--- get_function.c
+++ get_function.c
@@ -1,9 +1,8 @@
 #include  "holberton.h"
 /**
- *
- *
- *
- *
+ * op_character -  Fuction that print all character
+ * @list: Have print arguments
+ * Return: Returns the characters
  */
 int op_character(va_list list)
 {
@@ -13,14 +12,13 @@ int op_character(va_list list)
 	return (1);
 }
 /**
- *
- *
- *
- *
+ * op_string - Fuction that print the string
+ * @list: Have print arguments
+ * Return: Returns the string
  */
 int op_string(va_list list)
 {
-	char* String = va_arg(list, char*);
+	char *String = va_arg(list, char*);
 	int length = 0;
 
 	if (String == NULL)
@@ -35,18 +33,17 @@ int op_string(va_list list)
 	write(1, String, length);
 	return (length);
 }
+/**
+ * op_percent - Fuction that print percent
+ * @list: Have print arguments
+ * Return: Returns the percent
+ */
+
 int op_percent(__attribute__((unused)) va_list list)
 {
 	char percent;
-	percent = '%';
 
+	percent = '%';
 	write(1, &percent, 1);
 	return (1);
 }
-/*int op_integer(va_list list)
-{
-	int Integer = va_arg(list, int);
-
-	write(1, &Integer, 1);
-	return (1);
-	}*/
