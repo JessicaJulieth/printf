8dfb39e2eec913a07f958a8f7256a851620ad938
JessicaJulieth <3497@holbertonschool.com>
2021-07-28 00:29:22 -0500

    Changes in the commits V2

--- get_op_func.c
+++ get_op_func.c
@@ -1,25 +1,23 @@
 #include "holberton.h"
 /**
- *
- *
+ * get_op_func - Function that contain the prototypes
+ * @Character: It is a pointer
+ * Return: none
  */
-
 int (*get_op_func(const char *Character))(va_list)
 {
-    op_t ops[] = {
-        {"c", op_character},
-	{"s", op_string},
-	{"d", op_integer},
-	{"i", op_integer},
-        {"%", NULL},
-        {NULL, NULL}
-    };
-    int i = 0;
-    while (ops[i].op != NULL)
-    {
-        if (*(ops[i].op) == *Character)
-            return (ops[i].function);
-        i++;
-    }
-    return (NULL);
+	op_t ops[] = {
+		{"c", op_character},
+		{"s", op_string},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (ops[i].op != NULL)
+	{
+		if (*(ops[i].op) == *Character)
+			return (ops[i].function);
+		i++;
+	}
+	return (NULL);
 }
