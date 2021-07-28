4544cfb05b7a8669c76bcfee2cec1e5fde623e8b
Julian Carlosama <cromocelcarlosama@gmail.com>
2021-07-27 22:20:30 -0500

    Functions that get each case V1

---
+++ get_function.c
@@ -0,0 +1,34 @@
+#include  "holberton.h"
+/**
+ *
+ *
+ *
+ *
+ */
+int op_character(va_list list)
+{
+	int Character = va_arg(list, int);
+
+	write(1, &Character, 1);
+	return (1);
+}
+/**
+ *
+ *
+ *
+ *
+ */
+int op_string(va_list list)
+{
+	int String = va_arg(list, int);
+
+	write(1, &String, 1);
+	return (1);
+}
+/*int op_integer(va_list list)
+{
+	int Integer = va_arg(list, int);
+
+	write(1, &Integer, 1);
+	return (1);
+	}*/
