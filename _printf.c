6855221d034f898dd50acea1b81d79e427672926
Julian Carlosama <cromocelcarlosama@gmail.com>
2021-07-29 16:08:48 -0500

    Add news funtions and corrections V 5

--- _printf.c
+++ _printf.c
@@ -1,44 +1,49 @@
 #include "holberton.h"
-
 /**
- * _printf - Functions that print all character
- * @format: Is a character string
- * Return: Output the length
-*/
-
+         * _printf - Functions that print all character
+         * @format: Is a character string
+         * Return: Output the length
+        */
 int _printf(const char *format, ...)
 {
-	unsigned long lenght_fmt = 0;
-	const char *fmt = format;
-	va_list list;
-	int (*f)(va_list);
+    unsigned long lenght_fmt = 0;
+    const char *fmt = format;
+    int x = 0;
+    int s = 0;
 
-	va_start(list, format);
-	while (fmt && *fmt)
-	{
-		if (*fmt == '%')
-		{
-			/* Analizamos el segundo carácter del %*/
-			/* por ejemplo: %d,  analizamos el cáracter d*/
-			fmt++;
-			f = get_op_func(fmt);
-			if (f != NULL)
-			{
-				lenght_fmt += f(list);
-			}
-			else
-			{
-				write(1, &fmt, 1);
-			}
-			fmt++;
-		}
-		else
-		{
-			lenght_fmt++;
-			write(1, fmt, 1);
-			fmt++;
-		}
-	}
-	va_end(list);
-	return (lenght_fmt);
+    va_list list;
+    int (*f)(va_list);
+
+    va_start(list, format);
+    s = percentcases(fmt, list);
+    if (s == (-1))
+    {
+        return (-1);
+    }
+    while (fmt[x])
+    {
+        if (fmt[x] == '%')
+        {
+            f = get_op_func(&fmt[x]);
+            if (f != NULL)
+            {
+                lenght_fmt += f(list);
+                x += 2;
+                continue;
+            }
+            else
+            {
+                if (fmt[x] == '%' && fmt[x + 1] == '\0')
+                {return (-1);
+                }
+                lenght_fmt += _putchar(fmt[x]);
+            }
+        }
+        else
+        {lenght_fmt += _putchar(fmt[x]);
+        }
+        x++;
+    }
+    va_end(list);
+    return (lenght_fmt);
 }
